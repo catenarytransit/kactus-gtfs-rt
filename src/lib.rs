f38fea9e9395441ce09c1409ffb7b14894ddaa96
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-11-06 09:17:54 -0800

    maybe its opening too many redis connections

--- src/lib.rs
+++ src/lib.rs
@@ -90,8 +90,8 @@ pub mod aspen {
         useexistingdata: bool,
     ) {
         //send data to aspen over tarpc
-        let redisclient = redis::Client::open("redis://127.0.0.1:6379/").unwrap();
-        let _con = redisclient.get_connection().unwrap();
+      //  let redisclient = redis::Client::open("redis://127.0.0.1:6379/").unwrap();
+        //let _con = redisclient.get_connection().unwrap();
     
         let generating_vehicles = [
             "f-mta~nyc~rt~mnr",
