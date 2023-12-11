218d9a9970c8303a6ec6c66f695a588b6202ea34
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-12-11 14:50:03 -0800

    add gtfs structures

--- src/conversions/amtrak/main.rs
+++ src/conversions/amtrak/main.rs
@@ -8,16 +8,22 @@ use kactus::insert::insert_gtfs_rt_bytes;
 
 use kactus::aspen::send_to_aspen;
 
+use gtfs_structures::Gtfs;
+
 #[tokio::main]
 async fn main() {
     let redisclient = RedisClient::open("redis://127.0.0.1:6379/").unwrap();
     let mut con = redisclient.get_connection().unwrap();
 
+    let gtfs = Gtfs::from_url_async("https://content.amtrak.com/content/gtfs/GTFS.zip")
+    .await
+    .unwrap();
+
     let client = reqwest::Client::new();
     loop {
-        let amtrak_raw_data = amtrak_gtfs_rt::fetch_amtrak_gtfs_rt(&client).await.unwrap();
+        let amtrak_gtfs_rt = amtrak_gtfs_rt::fetch_amtrak_gtfs_rt(&gtfs, &client).await.unwrap();
 
-        let vehicle_data = amtrak_raw_data.vehicle_positions.encode_to_vec();
+        let vehicle_data = amtrak_gtfs_rt.vehicle_positions.encode_to_vec();
 
         insert_gtfs_rt_bytes(
             &mut con,
